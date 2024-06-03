fa4dbd1a96e83801105f33cbdda3d07e04b3694e
marin <marin@STERBLICHEN>
2024-06-02 22:42:52 -0400

    Agregar archivos de proyecto.

---
+++ RegistroDeColegio/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace RegistroDeColegio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRegistrar = new System.Windows.Forms.Button();
+            this.BtnActualizar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.TBNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnRegistrar
+            // 
+            this.BtnRegistrar.Location = new System.Drawing.Point(85, 290);
+            this.BtnRegistrar.Name = "BtnRegistrar";
+            this.BtnRegistrar.Size = new System.Drawing.Size(81, 20);
+            this.BtnRegistrar.TabIndex = 0;
+            this.BtnRegistrar.Text = "Registrar";
+            this.BtnRegistrar.UseVisualStyleBackColor = true;
+            this.BtnRegistrar.Click += new System.EventHandler(this.BtnRegistrar_Click);
+            // 
+            // BtnActualizar
+            // 
+            this.BtnActualizar.Location = new System.Drawing.Point(211, 290);
+            this.BtnActualizar.Name = "BtnActualizar";
+            this.BtnActualizar.Size = new System.Drawing.Size(98, 20);
+            this.BtnActualizar.TabIndex = 1;
+            this.BtnActualizar.Text = "Actualizar";
+            this.BtnActualizar.UseVisualStyleBackColor = true;
+            this.BtnActualizar.Click += new System.EventHandler(this.BtnActualizar_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(358, 290);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(74, 20);
+            this.BtnEliminar.TabIndex = 2;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // TBNombre
+            // 
+            this.TBNombre.Location = new System.Drawing.Point(189, 107);
+            this.TBNombre.Name = "TBNombre";
+            this.TBNombre.Size = new System.Drawing.Size(136, 20);
+            this.TBNombre.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Datos Alumnos";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Edad:";
+            // 
+            // TBEdad
+            // 
+            this.TBEdad.Location = new System.Drawing.Point(189, 162);
+            this.TBEdad.Name = "TBEdad";
+            this.TBEdad.Size = new System.Drawing.Size(136, 20);
+            this.TBEdad.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fecha de nacimiento:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(189, 219);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(114, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Numero de estudiante:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBEdad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBNombre);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.BtnActualizar);
+            this.Controls.Add(this.BtnRegistrar);
+            this.MaximumSize = new System.Drawing.Size(600, 400);
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRegistrar;
+        private System.Windows.Forms.Button BtnActualizar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.TextBox TBNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ RegistroDeColegio/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegistroDeColegio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text.Trim();
+            string nombre = TBNombre.Text.Trim();
+            string edad = TBEdad.Text.Trim();
+            DateTime fecha = dateTimePicker1.Value;
+            string fecha_1 = fecha.ToString();
+
+            string query = "Insert INTO dbo.login(id,nombreAlumno,edad,fechaDeNacimiento)values( "+ id +" ,'" + nombre + "','" + edad + "','" + fecha_1 + "')";
+            try
+            {
+                SqlConnection conexion = new SqlConnection("server = STERBLICHEN;database=Login;integrated security = true");
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                int resultado = cmd.ExecuteNonQuery();
+                if (resultado == 0)
+                {
+                    MessageBox.Show("Registro no insertado");
+                }
+                else
+                {
+                    MessageBox.Show("Registro agregado exitosamente");
+                }
+                conexion.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnActualizar_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text.Trim();
+            string nombre = TBNombre.Text.Trim();
+            string edad = TBEdad.Text.Trim();
+            DateTime fecha = dateTimePicker1.Value;
+            string fecha_1 = fecha.ToString();
+
+            string query = "UPDATE dbo.login SET nombreAlumno = '"+nombre+"', edad = '"+edad+"', fechaDeNacimiento = '"+fecha_1+"' where id = "+id+" ";
+
+            try
+            {
+                SqlConnection conexion = new SqlConnection("server = STERBLICHEN;database=Login;integrated security = true");
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                int resultado = cmd.ExecuteNonQuery();
+                if (resultado == 0)
+                {
+                    MessageBox.Show("Registro no insertado");
+                }
+                else
+                {
+                    MessageBox.Show("Se Actualizo exitosamente");
+                }
+                conexion.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+
+            string id = textBox1.Text.Trim();
+            string nombre = TBNombre.Text.Trim();
+            string edad = TBEdad.Text.Trim();
+            DateTime fecha = dateTimePicker1.Value;
+            string fecha_1 = fecha.ToString();
+            try
+            {
+                string query = "DELETE FROM login WHERE id = " + id + "";
+
+                SqlConnection conexion = new SqlConnection("server = STERBLICHEN;database=Login;integrated security = true");
+                conexion.Open();
+                SqlCommand cmd = new SqlCommand(query, conexion);
+                int resultado = cmd.ExecuteNonQuery();
+                if (resultado == 0)
+                {
+                    MessageBox.Show("Registro no insertado");
+                }
+                else
+                {
+                    MessageBox.Show("Se Elimino exitosamente");
+                }
+                conexion.Close();
+
+            }
+            catch (Exception ex)
+            {
+               
+            }
+        }
+    }
+}
